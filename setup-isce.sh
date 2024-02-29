3ba4a0b2de3fd6c53166b812f1b5e064fc5ee185
Zulfikar Akbar Muzakki <zakki@kartoza.com>
2024-02-29 12:37:10 +0000

    Update shell.nix

--- setup-isce.sh
+++ setup-isce.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 # Dev env for Wolfgangs SAR
+mkdir isce2-build
 git clone https://github.com/isce-framework/isce2
 cd isce2
 mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=../../isce2-build/
 # This line a dirty hack to solve the Screenwriter.h missing
-make 
+make -j47
 # Running again should fix it....
+cd ..
+cd ..
+cd isce2/build
 make install
 cd ..
 cd ..
--- shell.nix
+++ shell.nix
@@ -84,6 +84,7 @@ in pkgs.mkShell rec {
 
     echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${PROJECT_ROOT}/fringe-build/lib:${stdenv.cc.cc.lib}/lib/:${pkgs.lib.makeLibraryPath buildInputs}:${pkgs.stdenv.cc.cc.lib.outPath}" >> environment.sh
     echo "export PATH=$PATH:`pwd`/snaphu-build" >> environment.sh
+    echo "export PATH=$PATH:`pwd`/$ISCE_STACK/topsStack" >> environment.sh
     chmod +x environment.sh
     source environment.sh
     ./setup.sh

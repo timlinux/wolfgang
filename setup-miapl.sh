ec26b90e487113e1ca03fbe9ec6ba9c538a92c52
Zulfikar Akbar Muzakki <zakki@kartoza.com>
2024-03-06 09:43:49 +0700

    Update setup miaplpy

--- setup-miapl.sh
+++ setup-miapl.sh
@@ -3,4 +3,5 @@
 git clone --depth 1 -b V0.2.1 https://github.com/insarlab/MiaplPy.git
 cd MiaplPy
 python -m pip install .
+yes| cp -rf src/miaplpy/defaults/ $(python -c 'import site; print(site.getsitepackages()[0])')/miaplpy/
 cd ..

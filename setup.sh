32217476cb0e9cce8867cd89ff6f7b279fec3210
Zulfikar Akbar Muzakki <zakki@kartoza.com>
2024-03-19 07:52:48 +0700

    Unset topsStack in shell.nix

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@
 ./setup-isce.sh  
 ./setup-miapl.sh
 ./setup-snaphu.sh
-sleep 5
-./setup-isce.sh
+
+# Copy patches to pyaps3 to support concurrent ERA5 download
 cp patches/pyaps3_autoget.py .venv/lib/python3.11/site-packages/pyaps3/autoget.py

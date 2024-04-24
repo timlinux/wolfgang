63a8c6a211a098082d9834db491bc8cca8b01214
Zulfikar Akbar Muzakki <zakki@kartoza.com>
2024-04-24 01:48:46 +0000

    Update jupyter-lab-script

--- jupyter-lab.sh
+++ jupyter-lab.sh
@@ -5,4 +5,5 @@ echo "after opening in its own window"
 echo "--------------------------------"
 python -m ipykernel install --user --name=virtualenv
 jupyter kernelspec list
-jupyter-lab
+IP=$(ip addr | grep inet | grep -v inet6 | tail -1 | grep -o "inet [0-9\.]*" | sed 's/inet //g')
+jupyter-lab --ip=${IP}
